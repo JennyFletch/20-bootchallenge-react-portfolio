58f6c90f6f5254aabcd2f5aaf31f181dbe5235ec
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2023-02-19 14:15:47 -0700

    Troubleshooting router issue with gh pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './normalize.css';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/pages/About';
@@ -17,7 +17,7 @@ function App() {
         <Header />
           
           <Routes>
-            <Route path="/" element={<About />} />
+            <Route exact path="/" component={<About />} />
             <Route path='/about' element={<About />} />
             <Route path='/portfolio' element={<Work />} />
             <Route path='/contact' element={<Contact />} />
